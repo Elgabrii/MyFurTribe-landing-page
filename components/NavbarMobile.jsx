a6cbc3f59b8d2fab1d6241e5af9c96a7dbebc295
Ahmad Elgabri <ahmedlg46@gmail.com>
2022-11-02 13:34:13 +0200

    removed links on mobile

--- components/NavbarMobile.jsx
+++ components/NavbarMobile.jsx
@@ -59,7 +59,8 @@ const NavBarMobile = () => {
       }
     </Button>
     {
-      isMenuOpen && <NavigationMenuMobile />
+      isMenuOpen && ''
+      // <NavigationMenuMobile />
     }
   </Box>
 }
